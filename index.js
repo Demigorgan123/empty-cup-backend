fb125efc3cf0f92f77e8840bd6349c4eb33f79a5
Demigorgan123 <88244827+Demigorgan123@users.noreply.github.com>
2023-11-28 17:43:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const {MongoClient} = require('mongodb')
 const cors = require('cors')
-const db_url = process.env.MONGODB_URL
+const db_url = `mongodb+srv://Dhanishkumar:${process.env.PASS_WD}@cluster0.snxi3hb.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(db_url)
 const app = express()
 app.use(cors({
     origin:"*"
 }))
-console.log(process.env)
 app.get('/', async (req, resp)=>{
     const connect = await client.connect()
     const db = connect.db('MyDB')
