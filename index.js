da3ba94b0a82a1f9ca16e4bee5320a8ea596719a
Demigorgan123 <88244827+Demigorgan123@users.noreply.github.com>
2023-11-28 16:31:44 +0530

    Add files via upload
    Empty cup backend

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const {MongoClient} = require('mongodb')
+const cors = require('cors')
+const db_url = process.env.MONGODB_URL
+const client = new MongoClient(db_url)
+const app = express()
+app.use(cors({
+    origin:"*"
+}))
+app.get('/', async (req, resp)=>{
+    const connect = await client.connect()
+    const db = connect.db('MyDB')
+    const coll = db.collection('Designers')
+    const res = await coll.find().toArray();
+    resp.send(res);
+    
+})
+app.listen(process.env.PORT, ()=>console.log('listening on port 5000...'))
